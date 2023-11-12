c46063b2456530c471ecd832359e1901383d840d
Tarik Dorhmi <tarikdorhmi@yahoo.fr>
2023-11-12 22:43:51 +0100

    test(Part III): Write Unit test

---
+++ laravel-cc/tests/Feature/ProductTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    /** @test */
+    public function a_product_can_be_created()
+    {
+        $response = $this->post('/api/products', [
+            'name' => 'Test Product 2',
+            'description' => 'This is a test product',
+            'price' => 49.99,
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('products', [
+            'name' => 'Test Product 2',
+            'description' => 'This is a test product',
+            'price' => 49.99,
+        ]);
+    }
+}
